5cd1038a038820509b661ae79e9d12c1d2dffe06
Travis McNeill <tavistock91@gmail.com>
2014-11-29 23:37:45 -0500

    Got this shhtinking thing to work

--- README.md
+++ README.md
@@ -3,4 +3,3 @@ Cloud To Butt Plus
 
 Chrome extension that replaces text with something a little more Zizek.
 
-[Direct download of crx file](https://github.com/)
--- Source/content_script.js
+++ Source/content_script.js
@@ -32,32 +32,22 @@ function walk(node)
 function handleText(textNode) {
 	var v = textNode.nodeValue;
 
-  // Deal with the easy case
-  v = v.replace(/\b(T|t)he (C|c)loud/g, function(match, p1, p2, offset, string) {
-    // t - 7 = m
-    // c - 1 = b
-    m = String.fromCharCode(p1.charCodeAt(0) - 7);
-    b = String.fromCharCode(p2.charCodeAt(0) - 1);
-    return m + "y " + b + "utt";
+  v = v.replace(/\b[s|S][^h]/g, function(match, p1, p2, offset, string) {
+    return match[0] + "hh" + match[1];
   });
 
-  // Deal with private clouds
-  v = v.replace(/\b(P|p)rivate (C|c)loud/g, function(match, p1, p2, offset, string) {
-    // c - 1 = b
-    b = String.fromCharCode(p2.charCodeAt(0) - 1);
-    return b + "utt";
+  v = v.replace(/\b[s|S]h[^h]/g, function(match, p1, p2, offset, string) {
+    return match[0] + match[2];
   });
-  // Get the corner cases
-  if(v.match(/cloud/i)) {
-    // If we're not talking about weather
-    if(v.match(/PaaS|SaaS|IaaS|computing|data|storage|cluster|distributed|server|hosting|provider|grid|enterprise|provision|apps|hardware|software|/i)) {
-      v = v.replace(/(C|c)loud/gi, function(match, p1, offset, string) {
-        // c - 1 = b
-        b = String.fromCharCode(p1.charCodeAt(0) - 1);
-        return b + "utt";
-      });
-    }
-  }
+
+  v = v.replace(/ss+/g, function(match, p1, p2, offset, string) {
+    return "s";
+  });
+
+ v = v.replace(/thish/g, function(match, p1, p2, offset, string) {
+    return "thish... thish";
+  });
+
 	textNode.nodeValue = v;
 }
 
